98957b1d94988d8aaa84c99814311a32cd60dd11
hacknetos <76149789+hacknetos@users.noreply.github.com>
2022-01-28 07:47:06 +0100

    Projektdateien hinzufügen.

---
+++ SpaceInvaders/player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spaceinvaders
+{
+    internal class Player
+    {
+        int pos;
+        String name;
+        int life;
+
+        public int Pos { get => pos; set => pos = value; }
+        public string Name { get => name; set => name = value; }
+        public int Life { get => life; set => life = value; }
+
+        public Player(int pos, string name, int life)
+        {
+            this.pos = pos;
+            this.name = name;
+            this.life = life;
+        }
+
+        public Player(int pos)
+        {
+            this.pos = pos;
+        }
+
+
+    }
+}
---
+++ SpaceInvaders/shoote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spaceinvaders
+{
+    internal class shoote
+    {
+        Boolean isEneimy;
+        int pos;//seitwerts
+        
+
+        public shoote(bool isEneimy, int pos)
+        {
+            this.isEneimy = isEneimy;
+            this.pos = pos;
+            
+        }
+
+        public int Pos { get => pos; set => pos = value; }
+    }
+}
