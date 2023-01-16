ce9b5d06ede152ab5e65d78b65f4e020a5548c2d
hacknetos <76149789+hacknetos@users.noreply.github.com>
2023-01-16 07:49:18 +0100

    Commplit reset

--- SpaceInvaders/Program.cs
+++ SpaceInvaders/Program.cs
@@ -5,19 +5,7 @@
         static public Boolean _RUNNING;
         public static async Task Main(string[] args)
         {
-            List<Enemies> enemies = new List<Enemies>();
-            for (int i = 0; i < 5; i++)
-            {
-                enemies.Add(new Enemies(0, i, 1));
-            }
-            Field map = new Field(5, 10, enemies, 5);
-            Player player = new Player(5, 5, "peter", map);
-            
-
-           map.mapBuilder();
-            _RUNNING = true;
-           Task.Run(map.mapOutput);          
-           player.move();
+          
             
 
 
